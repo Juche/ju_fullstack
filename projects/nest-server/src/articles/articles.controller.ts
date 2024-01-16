d69086c87c44f23943861538e4d8cb5bf2b37ffd
Juche <azhucheng1@qq.com>
2024-01-16 10:48:30 +0800

    feat: 为 Article 模型实现 CRUD 操作

--- projects/nest-server/src/articles/articles.controller.ts
+++ projects/nest-server/src/articles/articles.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common'
 import { ArticlesService } from './articles.service'
 import { CreateArticleDto } from './dto/create-article.dto'
 import { UpdateArticleDto } from './dto/update-article.dto'
@@ -15,6 +15,11 @@ export class ArticlesController {
   @Get()
   findAll() {
     return this.articlesService.findAll()
+  }
+
+  @Get('drafts')
+  findDrafts() {
+    return this.articlesService.findDrafts()
   }
 
   @Get(':id')
