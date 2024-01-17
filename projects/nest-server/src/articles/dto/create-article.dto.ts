1f6ceeffa1baedc87e8b4e78e3f0738395dad4a3
Juche <azhucheng1@qq.com>
2024-01-17 15:54:05 +0800

    feat: 创建文章的字段验证

--- projects/nest-server/src/articles/dto/create-article.dto.ts
+++ projects/nest-server/src/articles/dto/create-article.dto.ts
@@ -1,15 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator'
 
 export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   @ApiProperty()
     title: string
 
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  @MaxLength(100)
   @ApiProperty({ required: false })
     description?: string
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
     body: string
 
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({ required: false, default: false })
     published?: boolean = false
 
