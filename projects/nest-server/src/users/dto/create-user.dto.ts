6811b4f67402aeda3a95d6772c65d72aeaac4df4
Juche <azhucheng1@qq.com>
2024-01-18 18:13:50 +0800

    feat: User 数据模型的 CRUD 操作

--- projects/nest-server/src/users/dto/create-user.dto.ts
+++ projects/nest-server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsString, MinLength } from 'class-validator'
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+    name: string
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+    email: string
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  @ApiProperty()
+    password: string
+
+}
--- projects/nest-server/src/users/entities/user.entity.ts
+++ projects/nest-server/src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger'
+import { User } from '@prisma/client'
+
+export class UserEntity implements User {
+  @ApiProperty()
+    id: number
+
+  @ApiProperty()
+    createdAt: Date
+
+  @ApiProperty()
+    updatedAt: Date
+
+  @ApiProperty()
+    name: string
+
+  @ApiProperty()
+    email: string
+
+  // @ApiProperty()  // 省略 @ApiProperty 装饰器将只会在 Swagger 文档中隐藏 password 属性。该属性依然会被展示在响应体中
+  password: string
+
+}
